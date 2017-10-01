dc3d1afffe56e0888ba88662aa59e7aebdb1bf02
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-10-01 16:02:39 -0400

    MOAR

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from redis import Redis, RedisError
 import os
 import socket
 
-more pythons error
+1 more python error
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
