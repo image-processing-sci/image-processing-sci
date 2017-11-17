923670404129a76c50cf7349125dd2469c069a1c
Aayush Kumar <aayush@lawn-143-215-55-147.lawn.gatech.edu>
2017-11-17 14:32:01 -0500

    added comments and code cleanup

--- transformation/transform.py
+++ transformation/transform.py
@@ -4,6 +4,7 @@ import numpy as np
 def transform(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # pixel coordinates of calibration checkerbox corners in original picture
     myFrom = np.float32([
             [1042, 406],
             [1299, 406],
@@ -11,18 +12,24 @@ def transform(img):
             [356, 790],
         ])
 
+    # side length of a square
     sL = 40
+    # width and height of output transformed image
     tW = 2000
     tH = 2000
 
+    # pixel coordinates of the calibration checkerbox corners in output transformed image
     myTo = np.float32([
             [tW/2 - 3*sL, tH/2 - 20*sL],
             [tW/2 + 3*sL, tH/2 - 20*sL],
             [tW/2 + 3*sL, tH/2 + 20*sL],
             [tW/2 - 3*sL, tH/2 + 20*sL],
         ])
+
+    # Get transformation matrix
     tMat = cv2.getPerspectiveTransform(myFrom,myTo)
 
+    # warp input image with transformation matrix (use this to warp detected centroids)
     dst = cv2.warpPerspective(img, tMat, (tW, tW))
 
     return (tMat, dst)
