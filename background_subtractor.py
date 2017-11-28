b8dbd73e63dd8de9f6e2aba435d361dab911fae3
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-11-27 19:55:52 -0500

    have semi working code for velocity- need to apply transformation

--- background_subtractor.py
+++ background_subtractor.py
@@ -1,67 +1,131 @@
 import cv2
-import cv2 as cv
-import numpy as np
 import os
-import imutils
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
 
 # Open the video
-capture = cv2.VideoCapture('./final.mp4')
+
+capture = cv2.VideoCapture('big_files/final.mp4')
 size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+        int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
 # if the output mp4 already exists, delete it
 try:
-    os.remove('./output.mp4')
+    os.remove('outputs/output.mp4')
 except:
     pass
 
 # create a new output mp4
-video = cv2.VideoWriter('output.mp4',fourcc, 30.0,size)
-fgbg= cv2.createBackgroundSubtractorMOG2(varThreshold = 100, detectShadows = False)
+video = cv2.VideoWriter('outputs/output.mp4', fourcc, 30.0, size)
+fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=100, detectShadows=False)
 detector = cv2.SimpleBlobDetector_create()
 
 # background image we're doing right
-background = cv2.imread('./background.png')
+background = cv2.imread('big_files/background.png')
 background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+FRAMES_FOR_SPEED = 1
+SCALING_FACTOR = 1.0
+
+previous_frame_centers = []
+frame_count = 0
+
+
+def calc_euclidean_distance(current_center, previous_center):
+    x1, y1 = current_center
+    x2, y2 = previous_center
+    return ((x1 - x2) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def match_centers_across_frames(current_frame_centers, previous_frame_centers):
+    return_map = dict.fromkeys(range(len(current_frame_centers)))
+
+    parametrized_direction = None
+
+    for i in range(len(current_frame_centers)):
+        # start distance at inf
+        curr_min_dist = float('inf')
+        index_to_pop = None
+        for j in range(len(previous_frame_centers)):
+            # get euclidean distance
+            distance = calc_euclidean_distance(current_frame_centers[i], previous_frame_centers[j])
+
+            if curr_min_dist > distance:
+                curr_min_dist = distance
+                index_to_pop = j
+                x1, y1 = current_frame_centers[i]
+                x2, y2 = previous_frame_centers[j]
+                parametrized_direction = (x1 - x2, y1 - y2)
+
+        if index_to_pop:
+            previous_frame_centers.pop(index_to_pop)
+        #TODO: Apply scaling factor
+        return_map[i] = (current_frame_centers[i], curr_min_dist * 30.0/FRAMES_FOR_SPEED * SCALING_FACTOR, parametrized_direction)  # this is the speed in pixels per second
+    return return_map
+
 
 while True:
     # capture frame in video
     ret, img = capture.read()
-    if ret==True:
-        imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # use the background subtractor
-        fgbg.apply(background)
-        fgmask = fgbg.apply(imgray)
+    if ret == True:
+        if frame_count % FRAMES_FOR_SPEED == 0:
+            imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # use the background subtractor
+            fgbg.apply(background)
+            fgmask = fgbg.apply(imgray)
 
-        # Pre processing, which includes blurring the image and thresholding
-        threshold = 30
-        new_blur = cv2.GaussianBlur(fgmask,(25,25),0)
-        ret,thresh = cv2.threshold(new_blur, threshold, 255, cv2.THRESH_BINARY)
+            # Pre processing, which includes blurring the image and thresholding
+            threshold = 30
+            new_blur = cv2.GaussianBlur(fgmask, (25, 25), 0)
+            ret, thresh = cv2.threshold(new_blur, threshold, 255, cv2.THRESH_BINARY)
 
-        # Get the contours for the thresholded image
-        im2, cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # Get the contours for the thresholded image
+            im2, cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        blob_area_threshold = 700
-        # loop over the contours
-        for c in cnts:
-            area = cv2.contourArea(c)  # getting blob area to threshold
-            # compute the center of the contour
-            if area > blob_area_threshold:
-                M = cv2.moments(c)
-                # prevent divide by zer0
-                if M["m00"] != 0.0:
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
+            blob_area_threshold = 700
+            current_frame_centers = []  # will contain a list of the circle coordinates
 
-                    # draw the contour and center of the shape on the image
-                    cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
-                    cv2.circle(img, (cX, cY), 7, (0, 255, 0), -1)
-                    cv2.putText(img, "center", (cX - 20, cY - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            # cv2.imshow("test", img)
+            # loop over the contours
+            for c in cnts:
+                area = cv2.contourArea(c)  # getting blob area to threshold
+                # compute the center of the contour
+                if area > blob_area_threshold:
+                    M = cv2.moments(c)
+                    # prevent divide by zer0
+                    if M["m00"] != 0.0:
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
 
-    if(cv2.waitKey(27)!=-1):
+                        centers_xy_coordinates = (cX, cY)
+                        if frame_count % FRAMES_FOR_SPEED == 0:
+                            current_frame_centers.append(centers_xy_coordinates)
+
+                        # draw the contour and center of the shape on the image
+                        cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+                        cv2.circle(img, (cX, cY), 7, (0, 255, 0), -1)
+            # do this for every FRAMES_FOR_SPEED frames.
+            # np_current_frame_centers = np.array(current_frame_centers)
+            # np_previous_frame_centers = np.array(previous_frame_centers)
+            # np_current_frame_centers.searchsorted(np_previous_frame_centers, side='right')
+
+            current_frame_centers.sort(key=lambda x: -x[1])
+            previous_frame_centers.sort(key=lambda x: -x[1])  # sort the centers by closest to camera (bottom of the frame)
+            car_map = match_centers_across_frames(current_frame_centers,
+                                                  previous_frame_centers)  # need to return velocities of vehicles (speed + direction)
+            print(car_map)
+            for key in car_map:
+                center, speed, parametrized_direction = car_map[key]
+                cX, cY = center
+                cv2.putText(img, str(round(speed)), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            previous_frame_centers = current_frame_centers
+            # import ipdb;
+            # ipdb.set_trace()
+        cv2.imshow("test", img)
+
+        frame_count += 1
+
+    if (cv2.waitKey(27) != -1):
         break
 
 capture.release()
