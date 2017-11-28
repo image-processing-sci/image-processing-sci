b0b570e50330b1efac1c72a4df64566482a6e3a8
SujeethJinesh <sujeethjinesh@gmail.com>
2017-11-27 21:17:33 -0500

    Finish transforming points

--- background_subtractor.py
+++ background_subtractor.py
@@ -1,10 +1,9 @@
 import cv2
 import os
-from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from transformation import transform
 
 # Open the video
-
 capture = cv2.VideoCapture('big_files/final.mp4')
 size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
@@ -26,6 +25,9 @@ background = cv2.imread('big_files/background.png')
 background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
 FRAMES_FOR_SPEED = 1
 SCALING_FACTOR = 1.0
+
+checkerboard_image = cv2.imread('betterCheckb.png')
+transformation_matrix, _ = transform(checkerboard_image)
 
 previous_frame_centers = []
 frame_count = 0
@@ -64,12 +66,35 @@ def match_centers_across_frames(current_frame_centers, previous_frame_centers):
     return return_map
 
 
+def showBirdsEyeCenters(centers):
+    # centers[:,2] = 0
+    # centers = np.hstack((centers, np.ones((centers.shape[0], 1))))
+
+    output = np.zeros((2000, 2000))
+    # apply t_mat to the centers and then plot those onto output
+    # import ipdb; ipdb.set_trace()
+    centers = cv2.perspectiveTransform(np.array([centers], dtype=float), transformation_matrix)
+    # print(centers)
+
+    # import ipdb; ipdb.set_trace()
+
+
+    for x, y in centers[0]:
+        # output[x,y] = 255
+        cv2.circle(output, (int(x), int(y)), 20, (255, 255, 255), -1)
+
+    cv2.imshow('birds-eye', output)
+
+    # output = cv2.warpPerspective(img, tMat, (tW, tH))
+
+
 while True:
     # capture frame in video
     ret, img = capture.read()
     if ret == True:
         if frame_count % FRAMES_FOR_SPEED == 0:
             imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # transformed_image = transform(imgray)
             # use the background subtractor
             fgbg.apply(background)
             fgmask = fgbg.apply(imgray)
@@ -112,16 +137,20 @@ while True:
             previous_frame_centers.sort(key=lambda x: -x[1])  # sort the centers by closest to camera (bottom of the frame)
             car_map = match_centers_across_frames(current_frame_centers,
                                                   previous_frame_centers)  # need to return velocities of vehicles (speed + direction)
-            print(car_map)
+            # print(car_map)
             for key in car_map:
                 center, speed, parametrized_direction = car_map[key]
                 cX, cY = center
-                cv2.putText(img, str(round(speed)), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
+                if speed != float('inf'):
+                    cv2.putText(img, str(round(speed)), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                else:
+                    cv2.putText(img, str('NaN'), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             previous_frame_centers = current_frame_centers
-            # import ipdb;
-            # ipdb.set_trace()
-        cv2.imshow("test", img)
+
+        cv2.imshow("original footage with blob/centroid", img)
+
+        showBirdsEyeCenters(current_frame_centers)
+
 
         frame_count += 1
 
