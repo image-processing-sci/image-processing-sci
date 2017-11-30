44d90e6064f5d951760cd84989fab3105462f0d4
SujeethJinesh <sujeethjinesh@gmail.com>
2017-11-29 19:53:10 -0500

    Add parametrized directions of vehicles

--- background_subtractor.py
+++ background_subtractor.py
@@ -24,12 +24,13 @@ detector = cv2.SimpleBlobDetector_create()
 background = cv2.imread('big_files/background.png')
 background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
 FRAMES_FOR_SPEED = 1
-SCALING_FACTOR = 1.0
+SCALING_FACTOR = 0.06818181804 #miles/pixel
 
 checkerboard_image = cv2.imread('betterCheckb.png')
 transformation_matrix, _ = transform(checkerboard_image)
 
 previous_frame_centers = []
+previous_transformed_centers = []
 frame_count = 0
 
 
@@ -39,30 +40,33 @@ def calc_euclidean_distance(current_center, previous_center):
     return ((x1 - x2) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 
-def match_centers_across_frames(current_frame_centers, previous_frame_centers):
-    return_map = dict.fromkeys(range(len(current_frame_centers)))
+def match_centers_across_frames(current_frame_centers_in, previous_frame_centers_in):
+    return_map = dict.fromkeys(range(len(current_frame_centers_in)))
 
     parametrized_direction = None
 
-    for i in range(len(current_frame_centers)):
+    for i in range(len(current_frame_centers_in)):
         # start distance at inf
         curr_min_dist = float('inf')
         index_to_pop = None
-        for j in range(len(previous_frame_centers)):
+        for j in range(len(previous_frame_centers_in)):
             # get euclidean distance
-            distance = calc_euclidean_distance(current_frame_centers[i], previous_frame_centers[j])
+            try:
+                distance = calc_euclidean_distance(current_frame_centers_in[i], previous_frame_centers_in[j])
+            except:
+                import ipdb; ipdb.set_trace()
 
             if curr_min_dist > distance:
                 curr_min_dist = distance
                 index_to_pop = j
-                x1, y1 = current_frame_centers[i]
-                x2, y2 = previous_frame_centers[j]
-                parametrized_direction = (x1 - x2, y1 - y2)
+                x1, y1 = current_frame_centers_in[i]
+                x2, y2 = previous_frame_centers_in[j]
+                parametrized_direction = (x2 - x1, y2 - y1)
 
         if index_to_pop:
-            previous_frame_centers.pop(index_to_pop)
+            previous_frame_centers_in.pop(index_to_pop)
         #TODO: Apply scaling factor
-        return_map[i] = (current_frame_centers[i], curr_min_dist * 30.0/FRAMES_FOR_SPEED * SCALING_FACTOR, parametrized_direction)  # this is the speed in pixels per second
+        return_map[i] = (current_frame_centers_in[i], curr_min_dist * 30.0/FRAMES_FOR_SPEED, parametrized_direction)  # this is the speed in pixels per second
     return return_map
 
 
@@ -74,18 +78,21 @@ def showBirdsEyeCenters(centers):
     # apply t_mat to the centers and then plot those onto output
     # import ipdb; ipdb.set_trace()
     centers = cv2.perspectiveTransform(np.array([centers], dtype=float), transformation_matrix)
-    # print(centers)
 
     # import ipdb; ipdb.set_trace()
+    centers = centers.tolist()[0]
 
-
-    for x, y in centers[0]:
+    index = 0
+    for x, y in centers:
         # output[x,y] = 255
         cv2.circle(output, (int(x), int(y)), 20, (255, 255, 255), -1)
+        centers[index] = (x, y)
+        index += 1
 
     cv2.imshow('birds-eye', output)
 
     # output = cv2.warpPerspective(img, tMat, (tW, tH))
+    return centers
 
 
 while True:
@@ -109,6 +116,7 @@ while True:
 
             blob_area_threshold = 700
             current_frame_centers = []  # will contain a list of the circle coordinates
+            current_transformed_centers = []
 
             # loop over the contours
             for c in cnts:
@@ -135,22 +143,42 @@ while True:
 
             current_frame_centers.sort(key=lambda x: -x[1])
             previous_frame_centers.sort(key=lambda x: -x[1])  # sort the centers by closest to camera (bottom of the frame)
+
+            current_transformed_centers.sort(key=lambda x: -x[1])
+            previous_transformed_centers.sort(key=lambda x: -x[1])
+
+            current_transformed_centers = showBirdsEyeCenters(current_frame_centers)
+
+            # import ipdb; ipdb.set_trace()
+            transformed_car_map = match_centers_across_frames(current_transformed_centers,
+                                                              previous_transformed_centers)
+
             car_map = match_centers_across_frames(current_frame_centers,
                                                   previous_frame_centers)  # need to return velocities of vehicles (speed + direction)
+
             # print(car_map)
-            for key in car_map:
-                center, speed, parametrized_direction = car_map[key]
+            for original, transformed in zip(car_map, transformed_car_map):
+                center, _, parametrized_direction = car_map[original]
+                _, speed, transformed_parametrized_direction = transformed_car_map[transformed]
+                speed *= SCALING_FACTOR
                 cX, cY = center
+                # import ipdb; ipdb.set_trace()
                 if speed != float('inf'):
                     cv2.putText(img, str(round(speed)), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 else:
                     cv2.putText(img, str('NaN'), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                if parametrized_direction != None:
+                    parametrized_x, parametrized_y = parametrized_direction
+                    x_direction = cX + parametrized_x
+                    y_direction = abs(parametrized_y - cY)
+                    # import ipdb; ipdb.set_trace()
+                    cv2.arrowedLine(img, center, (y_direction, x_direction), (0, 0, 255), 2)
+
             previous_frame_centers = current_frame_centers
+            previous_transformed_centers = current_transformed_centers
 
         cv2.imshow("original footage with blob/centroid", img)
-
-        showBirdsEyeCenters(current_frame_centers)
-
 
         frame_count += 1
 
